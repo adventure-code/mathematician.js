93b155ee38c33a1a068aec8bdfe3811f51b1af61
emanoelvianna <emanoel.vianna@outlook.com>
2017-02-05 01:53:34 -0200

    setup - adicionado lib de teste de unidade

---
+++ test/mathematician-spec.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('Mathematician', function() {
+  describe('test method', function() {
+    it('test', function() {
+      assert.equal(true, true);
+    });
+  });
+});
